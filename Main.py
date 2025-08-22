83ad504e14554e7892fa1cfef8d89531bdceb582
FalconKiller2013 <hummariaz786@gmail.com>
2025-08-22 14:21:30 +0500

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,12 @@
+age=12
+grade=7
+name="Behrawer"
+print(age)
+print(grade)
+print(name)
+
+print("My hobbie")
+fravoritegame="Car X Drift racing 2"
+fravoritesport="Motor sports"
+print(fravoritegame)
+print(fravoritesport)
---
+++ Type cast.py
@@ -0,0 +1,10 @@
+num=25
+num2=30.52
+name="codigal"
+b=True
+print("Datatype of num is ",type(num))
+print("Datatype of num2 is ",type(num2))
+print("Datatype of name is ",type(name))
+print("Datatype of b is ",type(b))
+num3=float(num)
+print("datatype after typecast of num is: ",num3)
