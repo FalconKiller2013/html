3ff68bd140d9a03d18b7e5230a00fd83719f41f7
FalconKiller2013 <hummariaz786@gmail.com>
2025-08-25 19:36:02 +0500

    Add files via upload

---
+++ notes.py
@@ -0,0 +1,7 @@
+amount=int(input("enter amount to withdraw:  "))
+note1=amount//100
+note2=amount//50
+note3=amount//200
+print(note1,"notes are there in",amount)
+print(note2,"notes are there in",amount)
+print(note3,"notes are there in",amount)
