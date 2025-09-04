31152e97b027ef24303df476bfc7a0b527f3c7e8
FalconKiller2013 <hummariaz786@gmail.com>
2025-09-04 19:48:13 +0500

    Add files via upload

---
+++ Percentage.py
@@ -0,0 +1,11 @@
+maths=int(input("enter maths marks:  "))
+english=int(input("enter english marks:  "))
+science=int(input("enter science marks:  "))
+social=int(input("enter social marks:  "))
+coding=int(input("enter coding marks:  "))
+Total=maths+english+science+social+coding
+print("Total marks are:  ",Total)
+average=Total/5
+print("Average is:  ",average)
+percentage=(Total/500)*100
+print("Your percentage is:  ",percentage)
