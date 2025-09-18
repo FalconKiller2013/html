56e6b48021786404f941b67236de7f537e3bcd04
FalconKiller2013 <hummariaz786@gmail.com>
2025-09-18 19:19:34 +0500

    Add files via upload

---
+++ Count of a string.py
@@ -0,0 +1,9 @@
+string=input("Enter a word: ")
+char=input("Enter a letter to check its count:  ")
+i=0
+count=0
+while i < len(string):
+    if string[i]== char:
+        count=count+1
+    i=i+1
+print("The",count, "number of times in",string )
